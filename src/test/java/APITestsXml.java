de73c95ab1da5f40fd885ffb80e206854bc3676a
Yosuva.Arulanthu <Yosuva.Arulanthu@docomodigital.com>
2022-09-21 16:26:32 +0100

    xml sample added

---
+++ src/test/java/APITestsXml.java
@@ -0,0 +1,50 @@
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+import static io.restassured.RestAssured.given;
+
+
+public class APITestsXml {
+    SOAPXmlResponseParser soapResponseParser;
+    public APITestsXml()
+    {
+         soapResponseParser = new SOAPXmlResponseParser();
+    }
+    @Test(priority = 0)
+    public void post_test_create_user() {
+
+        RestAssured.baseURI="https://www.w3schools.com/xml/tempconvert.asmx";
+        Response response = given()
+                //.queryParam("name", "value")
+                //.queryParam("name", "value")
+                .filter(new CustomLogFilter())
+                .headers("SOAPAction", "https://www.w3schools.com/xml/FahrenheitToCelsius")
+                .contentType("text/xml")
+                .body("<?xml version=\"1.0\" encoding=\"utf-8\"?>\n" +
+                        "<soap:Envelope xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\" xmlns:xsd=\"http://www.w3.org/2001/XMLSchema\" xmlns:soap=\"http://schemas.xmlsoap.org/soap/envelope/\">\n" +
+                        "  <soap:Body>\n" +
+                        "   <FahrenheitToCelsius xmlns=\"https://www.w3schools.com/xml/\">\n" +
+                        "     <Fahrenheit>100</Fahrenheit>\n" +
+                        "   </FahrenheitToCelsius>\n" +
+                        "  </soap:Body>\n" +
+                        "</soap:Envelope>")
+                .when()
+                .post()
+                .then()
+                .extract().response();
+        System.out.println(response.body().asString());
+        Assert.assertEquals(response.statusCode(), 200);
+        System.out.println(soapResponseParser.getResponseValue(response,"FahrenheitToCelsiusResult"));
+        Assert.assertEquals(soapResponseParser.getResponseValue(response,"FahrenheitToCelsiusResult"), "37.7777777777778");
+        //response.path("Response.@type"); this is for normal xml response
+    }
+
+
+}
---
+++ src/test/java/SOAPXmlResponseParser.java
@@ -0,0 +1,62 @@
+import io.restassured.response.Response;
+import io.restassured.response.ResponseBody;
+import org.w3c.dom.Document;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+import org.xml.sax.InputSource;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import java.io.StringReader;
+
+public class SOAPXmlResponseParser {
+    public String getResponseValue(Response response, String node) {
+        ResponseBody body = response.getBody();
+        String xmlValue = body.asString();
+        Node parentChild =null ;
+        Document doc = convertStringToXMLDocument(xmlValue);
+        if (xmlValue.contains("soap:") ) {
+            parentChild = doc.getFirstChild().getFirstChild().getFirstChild();
+        }else
+        {
+            parentChild = doc.getFirstChild();
+
+        }
+        NodeList nodeList = parentChild.getChildNodes();
+        String nodeValue=null;
+        for (int j = 0; j < nodeList.getLength(); j++) {
+            Node childNode = nodeList.item(j);
+            if (childNode.getNodeType() == Node.ELEMENT_NODE) {
+                if(childNode.getNodeName().equals(node)){
+                    if(null!=childNode.getFirstChild()) {
+                        nodeValue = childNode.getFirstChild().getNodeValue();
+                        System.out.println("Node [%s] is present and has a value [%s]"+childNode.getNodeName()+" "+nodeValue);
+                    }else{
+                        System.out.println("Empty node is present");
+                    }
+                    break;
+                }
+            }else{
+                System.out.println("Node is not present");
+            }
+        }
+        return nodeValue;
+    }
+
+    private  Document convertStringToXMLDocument(String xmlString)
+    {
+        DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+        DocumentBuilder builder = null;
+        try
+        {
+            builder = factory.newDocumentBuilder();
+            Document doc = builder.parse(new InputSource(new StringReader(xmlString)));
+            return doc;
+        }
+        catch (Exception e)
+        {
+            e.printStackTrace();
+        }
+        return null;
+    }
+}
